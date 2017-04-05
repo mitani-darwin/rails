4c73e60091566e1dcc7f6f50ed012f66fedd36a5
三谷徹 <mitani@daisuke2.com>
2017-04-05 19:07:04 +0900

    やはり元に戻した。

--- site-cookbooks/rails/attributes/rails.rb
+++ site-cookbooks/rails/attributes/rails.rb
@@ -1,6 +1,2 @@
 default['rails']['user']['name'] = "vagrant"
-if attribute?('rails_version')
-    default['rails']['version'] = node['rails_version']
-else
-    default['rails']['version'] = "5.0"
-end
+default['rails']['version'] = "5.0"
